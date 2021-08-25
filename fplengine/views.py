ece06a9039672a2b18f42530440ef35ae3b421ec
Aryan Sainju <aryan.sainju@gmail.com>
2021-08-25 18:25:37 +0000

    sd

--- fplengine/views.py
+++ fplengine/views.py
@@ -428,7 +428,7 @@ def get_gameweek_winner():
     
     return HttpResponse(f"{gameweek_winner_point.player.name}")
 
-# get_gameweek_winner()
+get_gameweek_winner()
 # def get_division_gameweek_winner():
 #     latest_gameweek = get_latest_gameweek()
 #     Gameweeks=Gameweek.objects.filter(player__id=51)
@@ -526,65 +526,6 @@ def get_newentries_classicleagues():
         # extract 'history_past' data from response into dataframe
         return r
 
-
-
-def get_gw_history():
-
-    players=Teams.objects.all()
-    for player in players:
-        player_id=player.entry
-            
-        '''get all past season info for a given player_id'''
-        
-        # send GET request to
-        # https://fantasy.premierleague.com/api/element-summary/{PID}/
-        r = requests.get(
-                base_url + 'entry/' + str(player_id) + '/history/'
-        ).json()
-
-        print(r)
-        # extract 'history_past' data from response into dataframe
-        for gw in r['current']:
-            gameweeek=gw['event']
-            points=gw['points']
-            point_on_bench=gw['points_on_bench']
-            totalpoints=gw['total_points']
-            inbank=gw['bank']
-            bank=inbank/10      
-            value=gw['value']
-            new_value=value/10
-            event_transfers=gw['event_transfers']
-            event_transfers_cost=gw['event_transfers_cost']
-
-        # for gw in r['past']:
-
-        # for chip in r['chips']:
-        #     name=chip['name']
-        #     time=chip['time']
-        #     event=chip['event']
-
-            try:
-                obj=Gameweek.objects.get(no=gameweeek,player__entry=player_id)
-                print(player.name+str(gameweeek)+'Data already exists')
-            except Gameweek.DoesNotExist:
-                gameweek=Gameweek.objects.create(
-                    no=gameweeek,
-                    point=points,
-                    point_on_bench=point_on_bench,
-                    totalpoints=totalpoints,
-                    bank=bank,
-                    value=new_value,
-                    event_transfers=event_transfers,
-                    event_transfers_cost=event_transfers_cost
-                    )
-                player=get_object_or_404(Teams,entry=player_id)
-                gameweek.player=player
-                gameweek.save()
-                # gameweek.player.add(player)
-                print(player.name + "-Gameweek:" + str(gameweeek) + "-"+ str(points))
-
-# get_gw_history()
-
 def get_results(r):
     for standing in r['standings']['results']:
             user = get_object_or_404(Teams,entry=standing['entry'])
@@ -597,38 +538,3 @@ def get_results(r):
                 print("updated")
         
 
-<<<<<<< HEAD
-# def get_classic_league():
-#     url = "https://fantasy.premierleague.com/api/leagues-classic/188305/standings"
-#     r = requests.get(url).json()
-#     data=str(r['standings']['has_next'])
-
-#     if data == "True":
-#         firsturl=url
-#         firstpageresult = requests.get(firsturl).json()
-#         secondurl="https://fantasy.premierleague.com/api/leagues-classic/188305/standings/?page_standings=2"
-#         secondpageresult = requests.get(secondurl).json()
-#         get_results(firstpageresult)
-#         get_results(secondpageresult)
-#     else:
-#       get_results(r)
-
-# get_classic_league()
-=======
-def get_classic_league():
-    url = "https://fantasy.premierleague.com/api/leagues-classic/188305/standings"
-    r = requests.get(url).json()
-    data=str(r['standings']['has_next'])
-
-    if data == "True":
-        firsturl=url
-        firstpageresult = requests.get(firsturl).json()
-        secondurl="https://fantasy.premierleague.com/api/leagues-classic/188305/standings/?page_standings=2"
-        secondpageresult = requests.get(secondurl).json()
-        get_results(firstpageresult)
-        get_results(secondpageresult)
-    else:
-      get_results(r)
-
-# get_classic_league()
->>>>>>> 7a6c48f8194cca290c5f02bacb4e1b20dc1075e4
